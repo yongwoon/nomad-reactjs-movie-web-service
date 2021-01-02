38c8ec4bdeae1bebab4a9de9ed96ff21e09d7009
yongwoon <yonyon.japan@gmail.com>
2021-01-02 13:04:44 +0900

    2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 
+// Food({fav}) -> es6
+function Food({fav}) {
+
+  console.log(fav);
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div>
-
+      <Food fav="kimchi" />
     </div>
   );
 }
